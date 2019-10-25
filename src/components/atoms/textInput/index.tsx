2f387ec42129ad01d0abd6b734ab0a67ba79bfd0
Alexandre Disdier <28678511+alexdisdier@users.noreply.github.com>
2019-10-25 07:09:14 +0200

    Created a textInput

---
+++ src/components/atoms/textInput/index.tsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import injectSheet, { ClassNameMap } from 'react-jss';
+
+import {
+  STATUS_INVALID,
+  STATUS_CAUTION,
+  STATUS_VALID
+} from '../../../constant/status';
+
+import styles from './textInput.style';
+
+interface Props {
+  classes: ClassNameMap<string>;
+  id?: string;
+  value: string;
+  type: 'text' | 'password' | 'search';
+  placeholder?: string;
+  handleFocus?: (e) => void;
+  handleBlur?: (e) => void;
+  handleChange?: (e) => void;
+  inputRef?: any;
+  status?: 'invalid' | 'caution' | 'valid';
+  disabled?: boolean;
+  required?: boolean;
+}
+
+const TextInput: React.SFC<Props> = ({
+  classes,
+  id = null,
+  placeholder = null,
+  value,
+  type,
+  handleFocus = () => {},
+  handleBlur = () => {},
+  handleChange = () => {},
+  inputRef = () => {},
+  status = null,
+  disabled = false,
+  required = false
+}) => {
+  const [hasFocus, setHasFocus] = useState(false);
+  const [focusCounter, setFocusCounter] = useState(0);
+  let timer;
+
+  /**
+   * the regex checks for a line with only white spaces.
+   */
+  const invalid =
+    (value === '' || value.match(/^[ \t\r\n\s]*$/g)) &&
+    focusCounter > 0 &&
+    required;
+
+  const focusHandler = e => {
+    window.clearTimeout(timer);
+    setHasFocus(true);
+    handleFocus!(e);
+  };
+
+  const blurHandler = e => {
+    window.clearTimeout(timer);
+    timer = window.setTimeout(() => {
+      setHasFocus(false);
+      if (required) setFocusCounter(focusCounter + 1);
+      handleBlur(e);
+    }, 50);
+  };
+
+  const rootProps = {
+    className: classes.root,
+    'data-has-focus': hasFocus,
+    'data-has-value': !!value.length,
+    'data-is-invalid':
+      status === STATUS_INVALID || (invalid === null ? false : invalid),
+    'data-is-caution': status === STATUS_CAUTION,
+    'data-is-valid': status === STATUS_VALID,
+    'data-is-disabled': disabled
+  };
+
+  const inputProps = {
+    onFocus: focusHandler,
+    onBlur: blurHandler,
+    onChange: handleChange,
+    ref: inputRef,
+    disabled
+  };
+
+  return (
+    <>
+      <div {...rootProps}>
+        <input
+          id={id!}
+          className={classes.input}
+          type={type}
+          placeholder={placeholder!}
+          value={value}
+          {...inputProps}
+        />
+        {/* {invalid && <ErrorMessage text={errorMessage} />} */}
+      </div>
+    </>
+  );
+};
+
+export default injectSheet(styles)(TextInput);
--- src/style/globalStyle.ts
+++ src/style/globalStyle.ts
@@ -1,6 +1,20 @@
 export default {
   // Colors
+  colorBrand: '#3e9f92', // teal
+
   white: '#FFFFFF',
   black: '#222222',
-  red: '#B00020'
+  /**
+   * shades of grey ranging from
+   * dark to light
+   */
+  grey1: '#bbb9bf',
+  grey2: '#828185',
+  grey4: '#c0bfc2',
+  grey5: '#d6d5d8',
+  grey9: '#f8f8f8',
+
+  cautionOrange: '#fd5000',
+  validGreen: '#009e32',
+  warningRed: '#B00020'
 };
