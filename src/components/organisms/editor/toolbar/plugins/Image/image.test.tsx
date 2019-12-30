e9be85aff6fd50e4a42b831a60c4a737a852ede2
Alexandre Disdier <alexdisdier@AlexandresMBP2.home>
2019-12-30 21:29:17 +0100

    test(editor.image): add missing test

---
+++ src/components/organisms/editor/toolbar/plugins/Image/image.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { classesFromStyles } from '../../../../../../utils/tests';
+
+// eslint-disable-next-line import/no-unresolved
+import { Image } from '.';
+
+// eslint-disable-next-line import/no-unresolved
+import styles from './image.style';
+
+const classes = classesFromStyles(styles);
+
+describe('Image', () => {
+  let props;
+
+  beforeEach(() => {
+    props = {
+      classes,
+      src:
+        'https://www.tesla.com/sites/tesla/files/curatedmedia/model-s%402x.jpg'
+    };
+  });
+
+  it('renders full component', () => {
+    const wrapper = shallow(<Image {...props} />);
+    expect(wrapper).toMatchInlineSnapshot(`
+      <img
+        alt="https://www.tesla.com/sites/tesla/files/curatedmedia/model-s%402x.jpg"
+        className="class-from-style-root"
+        src="https://www.tesla.com/sites/tesla/files/curatedmedia/model-s%402x.jpg"
+      />
+    `);
+  });
+});
