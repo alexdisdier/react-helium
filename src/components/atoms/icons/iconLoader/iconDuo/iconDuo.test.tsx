af0f952d664a578ad7620f1a653bb8fb230e2bbd
Alexandre Disdier <28678511+alexdisdier@users.noreply.github.com>
2020-01-12 21:46:14 +0100

    fix test

--- src/components/atoms/icons/iconLoader/iconDuo/iconDuo.test.tsx
+++ src/components/atoms/icons/iconLoader/iconDuo/iconDuo.test.tsx
@@ -1,57 +1,58 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { classesFromStyles } from '../../../../../utils/tests';
+import React from "react";
+import { shallow } from "enzyme";
+import { classesFromStyles } from "../../../../../utils/tests";
 
-import { IconDuo } from '.';
+import { IconDuo } from ".";
 
-import styles from './iconDuo.style';
+import styles from "./iconDuo.style";
 
 const classes = classesFromStyles(styles);
 
-describe('IconDuo', () => {
+describe("IconDuo", () => {
   let props;
 
   beforeEach(() => {
     props = {
       classes,
       size: 32,
-      speed: '0.5',
-      repeatCount: 'indefinite'
+      speed: "0.5",
+      repeatCount: "indefinite"
     };
   });
 
-  it('renders a IconDuo', () => {
+  it("renders a IconDuo", () => {
     const wrapper = shallow(<IconDuo {...props} />);
     expect(wrapper).toMatchInlineSnapshot(`
-      <svg
-        aria-hidden="true"
+      <div
         className="class-from-style-root"
-        fillRule="evenodd"
-        style={
-          Object {
-            "height": "32px",
-            "width": "32px",
-          }
-        }
-        viewBox="0 0 50 50"
-        x="0px"
-        xmlns="http://www.w3.org/2000/svg"
-        y="0px"
       >
-        <path
-          d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z"
+        <div
+          className="class-from-style-duo"
+          data-is-duo1={true}
         >
-          <animateTransform
-            attributeName="transform"
-            attributeType="xml"
-            dur="0.5s"
-            from="0 25 25"
-            repeatCount="indefinite"
-            to="360 25 25"
-            type="rotate"
+          <div
+            className="class-from-style-dot"
+            data-is-dota={true}
           />
-        </path>
-      </svg>
+          <div
+            className="class-from-style-dot"
+            data-is-dotb={true}
+          />
+        </div>
+        <div
+          className="class-from-style-duo"
+          data-is-duo2={true}
+        >
+          <div
+            className="class-from-style-dot"
+            data-is-dota={true}
+          />
+          <div
+            className="class-from-style-dot"
+            data-is-dotb={true}
+          />
+        </div>
+      </div>
     `);
   });
 });
